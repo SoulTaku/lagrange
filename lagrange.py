b6530156bc26f718de95cfd32acc380648ee10c2
SoulTaku <alexmoga69@yahoo.com>
2019-01-08 11:10:15 +0200

    Added modulo support

--- lagrange.py
+++ lagrange.py
@@ -2,12 +2,30 @@
 from sage.all import *
 
 class Lagrange:
-    def __init__(self, xs, ys):
-        self.xs = xs
-        self.ys = ys
+    def __init__(self, xs, ys, mod=None):
+        self.xs     = xs
+        self.ys     = ys
+        self.mod    = mod
 
-        self.ps = []
-        self.x  = var('x')
+        self.ps     = []
+        self.x      = var('x')
+
+
+    def egcd(self, a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, y, x = self.egcd(b % a, a)
+            return (g, x - (b // a) * y, y)
+
+
+    def modinv(self, a, m):
+        g, x, y = self.egcd(a, m)
+        if g != 1:
+            raise Exception('modular inverse does not exist')
+        else:
+            return x % m
+
 
     def interpolate(self):
         for x1, y in zip(self.xs, self.ys):
@@ -29,5 +47,15 @@ class Lagrange:
 
         for i in g.coefficients():
             f = f + x**i[1] * i[0]
+
+        if mod is not None:
+            coeffs = f.coefficients()
+            f = 0
+
+            for i in coeffs:
+                f_numerator     = i[0].numerator()
+                f_denominator   = i[0].denominator()
+
+                f = f + x**i[1] * (int(f_numerator * self.modinv(int(f_denominator), int(self.mod))) % int(self.mod))
 
         return f
